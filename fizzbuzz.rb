e5121be527b79a8047ff0bbcabfaf5006e34dc19
Jalen Roberts <jalenmr94@hotmail.com>
2019-06-03 23:05:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,2 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(Integer)
-  if Integer % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  end
-end
